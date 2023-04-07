69c1eefc7dd8ca2dc15054e0ab56427ef63211cd
AlexandrBirman <birman.alexandr@gmail.com>
2023-04-07 11:26:16 +0300

    3.1.4 - isPalindrome - formating

--- fp-haskell-stepik-1/_3_1/isPalindrome.hs
+++ fp-haskell-stepik-1/_3_1/isPalindrome.hs
@@ -4,5 +4,5 @@ isPalindrome :: Eq a => [a] -> Bool
 isPalindrome []  = True 
 isPalindrome [x] = True
 isPalindrome xs
-    | head xs == last xs = isPalindrome $ init . tail $ xs
-    | otherwise = False 
+    | head xs == last xs  = isPalindrome $ init . tail $ xs
+    | otherwise           = False 
