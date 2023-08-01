e8a3e7f8a231b8cf17abe56be315646df9f5607e
AlexandrBirman <birman.alexandr@gmail.com>
2023-08-01 10:06:39 +0300

    3.2.3 qsort, it was painful

---
+++ fp-haskell-stepik-1/_3_2/qsort.hs
@@ -0,0 +1,8 @@
+qsort :: Ord a => [a] -> [a]
+
+qsort [] = []
+qsort (x:xs) = (qsort lesser) ++ [x] ++ (qsort greater)
+    where 
+        lesser  = filter (< x) xs
+        greater = filter (>= x) xs
+
