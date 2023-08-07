65dec3e75db790e565b569f5b5c012d88597372f
AlexandrBirman <birman.alexandr@gmail.com>
2023-08-07 16:24:58 +0300

    rename stepil haskell 1 directory for brevity

--- fp-haskell-stepik-1/_3_4/evenOnly.hs
+++
@@ -1,4 +0,0 @@
-evenOnly :: [a] -> [a]
-evenOnly = snd . foldr (\a (xs, ys) -> (a:ys, xs)) ([],[])
-
-
--- fp-haskell-stepik-1/_3_5/meanList.hs
+++
@@ -1,2 +0,0 @@
-meanList :: [Double] -> Double
-meanList = uncurry (/) . foldr (\x (sum, count) -> (sum + x, succ count)) (0, 0)
