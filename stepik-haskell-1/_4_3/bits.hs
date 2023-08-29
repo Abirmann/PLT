5d84b7a46d7255813f738a2493e420b34e7aacb1
AlexandrBirman <alexbirman513@gmail.com>
2023-08-29 18:54:11 +0300

    bits.hs - add function draft

--- stepik-haskell-1/_4_3/bits.hs
+++ stepik-haskell-1/_4_3/bits.hs
@@ -1,10 +1,39 @@
+-- Integer can be represented as a list of bits with sign.
+
+-- Implement functions of addition and multiplication of such numbers assuming
+-- that lower bits come first in the list and higher buts come last.
+-- You can assume that input cannot have numbers with leading zeroes.
+
 data Bit = Zero | One    deriving (Read, Show)
-data Sign = Minus | Plus deriving (Read, Show)
+data Sign = Minus | Plus deriving (Read, Show, Eq)
 data Z = Z Sign [Bit]    deriving (Read, Show)
 
 
 add :: Z -> Z -> Z
-add = const
+add (Z s1 b1) (Z s2 b2)
+    | s1 == s2  = Z s1 $ sameSignBitsAdd b1 b2 Zero
+    | otherwise = undefined
+    where
+        sameSignBitsAdd [] [] Zero = []
+        sameSignBitsAdd [] [] One = [One]
+
+        sameSignBitsAdd (b1:b1s) [] prevC = r : (sameSignBitsAdd b1s [] c) 
+        sameSignBitsAdd [] (b2:b2s) prevC = r : (sameSignBitsAdd b2s [] c)
+            where
+                r = fst $ addBitWithCarry b1 prevC Zero 
+                c = snd $ addBitWithCarry b2 prevC Zero
+
+        sameSignBitsAdd (b1:b1s) (b2:b2s) prevC = r : (sameSignBitsAdd b1s b2s c)
+            where 
+                r = fst $ addBitWithCarry b1 b2 prevC 
+                c = snd $ addBitWithCarry b1 b2 prevC
+
+        addBitWithCarry :: Bit -> Bit -> Bit -> (Bit, Bit)
+        addBitWithCarry One One c = (Zero, One)
+        addBitWithCarry One Zero c = (One, c)
+        addBitWithCarry Zero One c = (One, c)
+        addBitWithCarry Zero Zero c = (Zero, c)
+
 
 mul :: Z -> Z -> Z
 mul = undefined
