5966aa64648b369f7386b1ddf507485692a41287
AlexandrBirman <alexbirman513@gmail.com>
2023-09-07 20:27:38 +0300

    bits - add invert for normalized bit lists

--- stepik-haskell-1/_4_3/bits.hs
+++ stepik-haskell-1/_4_3/bits.hs
@@ -12,10 +12,9 @@ data Z = Z Sign [Bit]    deriving (Read, Show)
 add :: Z -> Z -> Z
 add (Z s1 b1) (Z s2 b2)
     | s1 == s2    = Z s1 $ sameSignBitsAdd (normalize (b1,b2)) Zero
-
-    -- TODO - инвертирование нормализованного значения и решение о знаке по последнему разряду
-    -- | s1 == Minus = Z s1 $ sameSignBitsAdd (normalize (b1,b2)) Zero 
-    -- | s2 == Minus = Z s2 $ sameSignBitsAdd (normalize (b1,b2)) Zero 
+    -- TODO - решение о знаке по последнему разряду
+    | s1 == Minus = Z s1 $ sameSignBitsAdd ((\(a,b) -> (a, map invert b)) $ normalize (b1,b2)) Zero 
+    | s2 == Minus = Z s2 $ sameSignBitsAdd ((\(a,b) -> (map invert a, b)) $ normalize (b1,b2)) Zero 
     where
         sameSignBitsAdd ([],[]) Zero = []
         sameSignBitsAdd ([],[]) One = [One]
