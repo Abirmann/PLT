91e1af7debd5962fc7dc7a648a7ba8f908ebcc43
AlexandrBirman <alexbirman513@gmail.com>
2023-09-12 19:57:43 +0300

    bits - refactor

--- stepik-haskell-1/_4_3/bits.hs
+++ stepik-haskell-1/_4_3/bits.hs
@@ -8,12 +8,10 @@ data Bit = Zero | One    deriving (Read, Show, Eq)
 data Sign = Minus | Plus deriving (Read, Show, Eq)
 data Z = Z Sign [Bit]    deriving (Read, Show, Eq)
 
-
 instance Ord Bit where
     compare One Zero = GT
     compare Zero One = LT
     compare _ _      = EQ
-
 
 compBits [] [] = EQ
 compBits [] (b:bs) = LT
@@ -25,6 +23,8 @@ compBits (b1:b1s) (b2:b2s)
         | length b1s < length b2s = LT  
         | b1 > b2   = GT
         | otherwise = LT 
+  
+
 
 add :: Z -> Z -> Z
 add (Z s1 b1) (Z s2 b2)
@@ -33,50 +33,49 @@ add (Z s1 b1) (Z s2 b2)
     | (compBits b1 b2) == GT  = Z s1 $ diffSignBitsAdd (normalize (b1,b2)) Zero
     -- не оптимально дважды разворачивать список ради удаления нулей
     | (compBits b1 b2) == LT  = Z s2 $ (reverse . dropWhile (== Zero) . reverse) (diffSignBitsAdd (normalize (b2,b1)) Zero)
+        where 
+            sameSignBitsAdd ([],[]) Zero = []
+            sameSignBitsAdd ([],[]) One = [One]
+            sameSignBitsAdd ((b1:b1s),(b2:b2s)) prevC = (fst resultAndCarry) : (sameSignBitsAdd (b1s,b2s) (snd resultAndCarry))
+                where 
+                    resultAndCarry = addBitsWithCarry b1 b2 prevC
+
+            diffSignBitsAdd ([],[]) extra = []
+            diffSignBitsAdd ((b1:b1s),(b2:b2s)) prevC = (fst resultAndBorrow) : (diffSignBitsAdd (b1s,b2s) (snd resultAndBorrow))
+                where 
+                    resultAndBorrow = substractBitsWithBorrow b1 b2 prevC
+
+            addBitsWithCarry :: Bit -> Bit -> Bit -> (Bit, Bit)
+            addBitsWithCarry a1 a2 c = 
+                case (a1,a2,c) of
+                    (One,One,One)    -> (One, One)
+                    (One,One,Zero)   -> (Zero, One)
+                    (One,Zero,One)   -> (Zero, One)
+                    (One,Zero,Zero)  -> (One, Zero)
+                    (Zero,One,One)   -> (Zero, One)
+                    (Zero,One,Zero)  -> (One, Zero)
+                    (Zero,Zero,One)  -> (One, Zero)
+                    (Zero,Zero,Zero) -> (Zero, Zero)
+           
+            substractBitsWithBorrow :: Bit -> Bit -> Bit -> (Bit, Bit)
+            substractBitsWithBorrow a1 a2 c = 
+                case (a1,a2,c) of
+                    (One,One,One)    -> (One,One) 
+                    (One,One,Zero)   -> (Zero,Zero)
+                    (One,Zero,One)   -> (Zero,Zero)
+                    (One,Zero,Zero)  -> (One, Zero)
+                    (Zero,One,One)   -> (Zero, One)
+                    (Zero,One,Zero)  -> (One, One)        
+                    (Zero,Zero,One)  -> (One, One)
+                    (Zero,Zero,Zero) -> (Zero, Zero)
+              
+            normalize = unzip . (uncurry norm)
+                where
+                    norm [] [] = []
+                    norm [] (x:xs) = (Zero,x) : norm [] xs
+                    norm (x:xs) [] = (x,Zero) : norm xs []
+                    norm (a:as) (b:bs) = (a,b) : norm as bs
     
-sameSignBitsAdd ([],[]) Zero = []
-sameSignBitsAdd ([],[]) One = [One]
-sameSignBitsAdd ((b1:b1s),(b2:b2s)) prevC = (fst resultAndCarry) : (sameSignBitsAdd (b1s,b2s) (snd resultAndCarry))
-    where 
-        resultAndCarry = addBitsWithCarry b1 b2 prevC
-
-diffSignBitsAdd ([],[]) extra = []
-diffSignBitsAdd ((b1:b1s),(b2:b2s)) prevC = (fst resultAndBorrow) : (diffSignBitsAdd (b1s,b2s) (snd resultAndBorrow))
-     where 
-         resultAndBorrow = substractBitsWithBorrow b1 b2 prevC
-
-addBitsWithCarry :: Bit -> Bit -> Bit -> (Bit, Bit)
-addBitsWithCarry a1 a2 c = 
-    case (a1,a2,c) of
-        (One,One,One)    -> (One, One)
-        (One,One,Zero)   -> (Zero, One)
-        (One,Zero,One)   -> (Zero, One)
-        (One,Zero,Zero)  -> (One, Zero)
-        (Zero,One,One)   -> (Zero, One)
-        (Zero,One,Zero)  -> (One, Zero)
-        (Zero,Zero,One)  -> (One, Zero)
-        (Zero,Zero,Zero) -> (Zero, Zero)
-       
-substractBitsWithBorrow :: Bit -> Bit -> Bit -> (Bit, Bit)
-substractBitsWithBorrow a1 a2 c = 
-    case (a1,a2,c) of
-        (One,One,One)    -> (One,One) 
-        (One,One,Zero)   -> (Zero,Zero)
-        (One,Zero,One)   -> (Zero,Zero)
-        (One,Zero,Zero)  -> (One, Zero)
-        (Zero,One,One)   -> (Zero, One)
-        (Zero,One,Zero)  -> (One, One)        
-        (Zero,Zero,One)  -> (One, One)
-        (Zero,Zero,Zero) -> (Zero, Zero)
-          
-normalize = unzip . (uncurry norm)
-    where
-        norm [] [] = []
-        norm [] (x:xs) = (Zero,x) : norm [] xs
-        norm (x:xs) [] = (x,Zero) : norm xs []
-        norm (a:as) (b:bs) = (a,b) : norm as bs
-
-
 
 mul :: Z -> Z -> Z
 mul = undefined
