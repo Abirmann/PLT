6eb8404df6b230285c9897a694c27f5b5baf4a80
AlexandrBirman <alexbirman513@gmail.com>
2023-09-12 19:42:26 +0300

    bits - full addition works

--- stepik-haskell-1/_4_3/bits.hs
+++ stepik-haskell-1/_4_3/bits.hs
@@ -9,23 +9,41 @@ data Sign = Minus | Plus deriving (Read, Show, Eq)
 data Z = Z Sign [Bit]    deriving (Read, Show, Eq)
 
 
+instance Ord Bit where
+    compare One Zero = GT
+    compare Zero One = LT
+    compare _ _      = EQ
+
+
+compBits [] [] = EQ
+compBits [] (b:bs) = LT
+compBits (b:bs) [] = GT
+compBits (b1:b1s) (b2:b2s) 
+        | b1 == b2  = compBits b1s b2s
+        -- не оптимально на каждом шаге рекурсии вычислять длинну. Нужно считать один раз
+        | length b1s > length b2s = GT
+        | length b1s < length b2s = LT  
+        | b1 > b2   = GT
+        | otherwise = LT 
+
 add :: Z -> Z -> Z
 add (Z s1 b1) (Z s2 b2)
-    | s1 == s2    = Z s1 $ sameSignBitsAdd (normalize (b1,b2)) Zero
-    -- TODO определять знак по доп. разряду 
-    | otherwise = Z s1 $ diffSignBitsAdd (normalize (b1,b2)) Zero
-    where
-        sameSignBitsAdd ([],[]) Zero = []
-        sameSignBitsAdd ([],[]) One = [One]
-        sameSignBitsAdd ((b1:b1s),(b2:b2s)) prevC = (fst resultAndCarry) : (sameSignBitsAdd (b1s,b2s) (snd resultAndCarry))
-            where 
-                resultAndCarry = addBitsWithCarry b1 b2 prevC
+    | s1 == s2 = Z s1 $ sameSignBitsAdd (normalize (b1,b2)) Zero
+    | b1 == b2 = Z Plus []
+    | (compBits b1 b2) == GT  = Z s1 $ diffSignBitsAdd (normalize (b1,b2)) Zero
+    -- не оптимально дважды разворачивать список ради удаления нулей
+    | (compBits b1 b2) == LT  = Z s2 $ (reverse . dropWhile (== Zero) . reverse) (diffSignBitsAdd (normalize (b2,b1)) Zero)
+    
+sameSignBitsAdd ([],[]) Zero = []
+sameSignBitsAdd ([],[]) One = [One]
+sameSignBitsAdd ((b1:b1s),(b2:b2s)) prevC = (fst resultAndCarry) : (sameSignBitsAdd (b1s,b2s) (snd resultAndCarry))
+    where 
+        resultAndCarry = addBitsWithCarry b1 b2 prevC
 
-        diffSignBitsAdd ([],[]) Zero = []
-        diffSignBitsAdd ([],[]) One = [One]
-        diffSignBitsAdd ((b1:b1s),(b2:b2s)) prevC = (fst resultAndBorrow) : (diffSignBitsAdd (b1s,b2s) (snd resultAndBorrow))
-            where 
-                resultAndBorrow = substractBitsWithBorrow b1 b2 prevC
+diffSignBitsAdd ([],[]) extra = []
+diffSignBitsAdd ((b1:b1s),(b2:b2s)) prevC = (fst resultAndBorrow) : (diffSignBitsAdd (b1s,b2s) (snd resultAndBorrow))
+     where 
+         resultAndBorrow = substractBitsWithBorrow b1 b2 prevC
 
 addBitsWithCarry :: Bit -> Bit -> Bit -> (Bit, Bit)
 addBitsWithCarry a1 a2 c = 
