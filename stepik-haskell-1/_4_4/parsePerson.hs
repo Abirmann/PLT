f00a83c5fd28fe1a6c69277c9414548b8d61eefd
AlexandrBirman <alexbirman513@gmail.com>
2023-09-26 18:17:20 +0300

    Person parser draft

---
+++ stepik-haskell-1/_4_4/parsePerson.hs
@@ -0,0 +1,8 @@
+data Error = ParsingError | IncompleteDataError | IncorrectDataError String
+
+data Person = Person { firstName :: String, lastName :: String, age :: Int }
+
+parsePerson :: String -> Either Error Person
+parsePerson = undefined  
+
+
