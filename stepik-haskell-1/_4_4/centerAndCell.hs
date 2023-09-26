4e48183a08131eba953637f0787c48a2588a3ada
AlexandrBirman <alexbirman513@gmail.com>
2023-09-26 16:18:15 +0300

    getCenter and getCell - the cost was a week...

---
+++ stepik-haskell-1/_4_4/centerAndCell.hs
@@ -0,0 +1,9 @@
+data Coord a = Coord a a deriving (Show, Read)
+
+getCenter :: Double -> Coord Int -> Coord Double
+getCenter size (Coord x y) = Coord (size * (fromIntegral x) + size / 2) (size * (fromIntegral y) + size / 2)
+
+getCell :: Double -> Coord Double -> Coord Int
+getCell size (Coord x y) = Coord (floor $ x / size) (floor $ y / size)  
+
+
