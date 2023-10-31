421efaa0fa24ab6946c6c19855e3af7895fcd138
AlexandrBirman <alexbirman513@gmail.com>
2023-10-31 20:42:22 +0300

    top level guards in parsePerson exercise

--- stepik-haskell-1/_4_4/parsePerson.hs
+++ stepik-haskell-1/_4_4/parsePerson.hs
@@ -3,7 +3,13 @@ data Error = ParsingError | IncompleteDataError | IncorrectDataError String
 data Person = Person { firstName :: String, lastName :: String, age :: Int }
 
 parsePerson :: String -> Either Error Person
-parsePerson = undefined  
+parsePerson string 
+    | hasWrongFormat fields = Left ParsongError
+    | size fields < 3        = Left IncompleteDataError 
+    -- дополнить кейсы - расписать сначала гарды, и лишь потом приступать к реализации функций  
+    where
+        fields = splitOn '\n' string
+        hasWrongFormat fields = undefined
 
 splitOn delimiter = foldr merge [[]] 
     where
