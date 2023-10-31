2e20dfb9d42ff0a3739b662189df490da980d255
AlexandrBirman <alexbirman513@gmail.com>
2023-10-31 19:59:55 +0300

    finished generic splitBy function
    I still do not know for sure how it works...

--- stepik-haskell-1/_4_4/parsePerson.hs
+++ stepik-haskell-1/_4_4/parsePerson.hs
@@ -5,4 +5,9 @@ data Person = Person { firstName :: String, lastName :: String, age :: Int }
 parsePerson :: String -> Either Error Person
 parsePerson = undefined  
 
-
+splitBy :: Char -> String -> [String]
+splitBy delimiter = foldr merge [[]] 
+    where
+        merge c (l:ls) 
+            | c == delimiter = [] : (l:ls) 
+            | c /= delimiter = (c:l):ls
