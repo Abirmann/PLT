fbd99ff122319868ebb28fbbccf91ad233820cd4
AlexandrBirman <alexbirman513@gmail.com>
2023-11-01 15:30:22 +0300

    refactor splitOn function... again

--- stepik-haskell-1/_4_4/parsePerson.hs
+++ stepik-haskell-1/_4_4/parsePerson.hs
@@ -3,13 +3,10 @@ data Error = ParsingError | IncompleteDataError | IncorrectDataError String
 data Person = Person { firstName :: String, lastName :: String, age :: Int }
 
 parsePerson :: String -> Either Error Person
-parsePerson string 
-    | hasWrongFormat fields = Left ParsingError
-    | length fields < 3        = Left IncompleteDataError 
-    -- дополнить кейсы - расписать сначала гарды, и лишь потом приступать к реализации функций  
-    where
-        fields = splitOn '\n' string
-        hasWrongFormat fields = undefined
+-- всю валидацию сделать через цепочку вида:
+-- validate3 . validate2 . validate1 $ Either Error [String]
+-- и подумать, как менять тип у Right конструктора
+parsePerson string = undefined 
 
 splitOn delimiter = foldr collect [[]] 
     where
