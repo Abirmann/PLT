0f49b3ff9f29552b82c42fa89595dc9ed17a47fe
AlexandrBirman <alexbirman513@gmail.com>
2023-11-01 20:16:24 +0300

    add Either chain logic in parsePerson function

--- stepik-haskell-1/_4_4/parsePerson.hs
+++ stepik-haskell-1/_4_4/parsePerson.hs
@@ -3,10 +3,22 @@ data Error = ParsingError | IncompleteDataError | IncorrectDataError String
 data Person = Person { firstName :: String, lastName :: String, age :: Int }
 
 parsePerson :: String -> Either Error Person
--- всю валидацию сделать через цепочку вида:
--- validate3 . validate2 . validate1 $ Either Error [String]
--- и подумать, как менять тип у Right конструктора
-parsePerson string = undefined 
+parsePerson string = createPerson . validateAge . getRequiedFields . getAllFields $ splitOn '\n' string
+
+
+getAllFields :: [String] -> Either Error [(String, String)]
+
+getAllFields [] = Right []
+getAllFields strings = undefined
+
+getRequiedFields :: Either Error [(String, String)] -> Either Error (String, String, String) 
+getRequiedFields = undefined
+
+validateAge :: Either Error (String, String, String) -> Either Error (String, String, String)
+validateAge = undefined
+
+createPerson :: Either Error (String, String, String) -> Either Error Person
+createPerson = undefined
 
 splitOn delimiter = foldr collect [[]] 
     where
